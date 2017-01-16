0050bc7c21b874cd6eb6cfa9ddafe775787afbd3
Sudarsana <sudaredd@gmail.com>
2017-01-16 10:32:49 -0500

    Add Rx

--- marketData/src/main/java/market/caching/repository/StockDataService.java
+++ marketData/src/main/java/market/caching/repository/StockDataService.java
@@ -1,25 +1,40 @@
 package market.caching.repository;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.annotation.PostConstruct;
 
 import market.data.download.StockDownLoad;
 import market.data.download.YahooDownloader;
 import market.data.model.Stock;
+import market.data.model.Symbol;
 import market.data.util.Utils;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import rx.Observable;
+import rx.Scheduler;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
 
 @Service
 public class StockDataService {
 
+	@Value("${batch.size:100}")
+	int batchSize;
+	
 	@Autowired
 	private StockDownLoad stockDownLoad;
 	
@@ -27,15 +42,45 @@ public class StockDataService {
 	 private CacheManager cacheManager;
 	
 	private static Logger log = Logger.getLogger(StockDataService.class);
+	
+	private ExecutorService executor = Executors.newFixedThreadPool(4);
 
 	@PostConstruct
 	public void init() {
 		log.info("load stocks");
-		process();
+		loadStocksWithRx();
 	}
 	
-	public void process() {
-		List<String> symbols = stockDownLoad.remoteSymbols();
+	public void loadStocksWithRx() {
+		Cache cache = cacheManager.getCache("stockCache");
+
+		List<String> symbols = remoteSymbols();
+		List<List<String>> subLists = Utils.sublist(symbols, batchSize);
+		
+		Observable<List<String>> symbs = Observable.from(subLists)
+										.onErrorResumeNext(readFromCsv());
+		
+		YahooDownloader downloader = new YahooDownloader();
+
+		symbs.
+		subscribeOn(Schedulers.from(executor)).
+		subscribe(syms-> {
+			try {
+				log.info("request symbols from yahoo:"+syms);
+				List<Stock> stocks = downloader.doYahooDownload(syms);
+				Observable.from(stocks)
+				.subscribe((Stock stock)-> {
+					cache.put(stock.getSymbol(), stock);
+				});
+				log.info("added symbols to cache:"+syms);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
+	
+	public void loadStocks() {
+		List<String> symbols = remoteSymbols();
 		List<List<String>> subLists = Utils.sublist(symbols, 100);
 		
 		YahooDownloader downloader = new YahooDownloader();
@@ -59,5 +104,20 @@ public class StockDataService {
 		Cache cache = cacheManager.getCache("stockCache");
 		return (Stock)cache.get(symbol);
 		//return null;
+	}
+	
+	private Observable<List<String>> readFromCsv() {
+		try {
+			List<String> symbols = Files.readAllLines(Paths.get("data/symbols.txt"));
+			return Observable.from(Utils.sublist(symbols, batchSize));
+		} catch (IOException e) {
+			log.error("error occured",e);
+		}
+		return null;
+	}
+
+	private List<String> remoteSymbols() {
+		List<String> symbols = stockDownLoad.remoteSymbols();
+		return symbols;
 	}
 }
