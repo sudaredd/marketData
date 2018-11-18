edcd7125b4e47286d5bba262eebb76d5c552e43a
sudaredd <sudaredd@gmail.com>
2018-11-18 16:13:44 -0500

    change

--- marketData/src/main/java/market/caching/config/HazelCacheConfig.java
+++ marketData/src/main/java/market/caching/config/HazelCacheConfig.java
@@ -2,7 +2,6 @@ package market.caching.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.EvictionPolicy;
@@ -14,24 +13,24 @@ public class HazelCacheConfig {
 
 	@Bean
 	public Config hazelCastConfig() {
-		 Config config = new Config();
-	        config.setInstanceName("hazelcast-cache");
+		Config config = new Config();
+		config.setInstanceName("hazelcast-cache");
 
-	        MapConfig allUsersCache = new MapConfig();
-	        allUsersCache.setTimeToLiveSeconds(2000);
-	        allUsersCache.setEvictionPolicy(EvictionPolicy.LFU);
-	        config.getMapConfigs().put("alluserscache",allUsersCache);
+		MapConfig allUsersCache = new MapConfig();
+		allUsersCache.setTimeToLiveSeconds(2000);
+		allUsersCache.setEvictionPolicy(EvictionPolicy.LFU);
+		config.getMapConfigs().put("alluserscache", allUsersCache);
 
-	        MapConfig usercache = new MapConfig();
-	        usercache.setTimeToLiveSeconds(2000);
-	        usercache.setEvictionPolicy(EvictionPolicy.LFU);
-	        config.getMapConfigs().put("usercache",usercache);
+		MapConfig usercache = new MapConfig();
+		usercache.setTimeToLiveSeconds(2000);
+		usercache.setEvictionPolicy(EvictionPolicy.LFU);
+		config.getMapConfigs().put("usercache", usercache);
 
-	        MapConfig stockCache = new MapConfig();
-	        usercache.setTimeToLiveSeconds(2000);
-	        usercache.setEvictionPolicy(EvictionPolicy.LFU);
-	        config.getMapConfigs().put("stockCache",stockCache);
-	        
-	        return config;
+		MapConfig stockCache = new MapConfig();
+		usercache.setTimeToLiveSeconds(2000);
+		usercache.setEvictionPolicy(EvictionPolicy.LFU);
+		config.getMapConfigs().put("stockCache", stockCache);
+
+		return config;
 	}
 }
--- marketData/src/main/java/market/caching/controller/StockController.java
+++ marketData/src/main/java/market/caching/controller/StockController.java
@@ -4,13 +4,7 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-import market.caching.repository.StockDataService;
-import market.data.model.Quote;
-import market.data.model.Sector;
-import market.data.model.Stock;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,29 +12,34 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
+import market.caching.repository.StockDataService;
+import market.data.model.Quote;
+import market.data.model.Sector;
+
 @RestController
 public class StockController {
 
 	@Autowired
-    private StockDataService stockDataService;
-	
-	    @RequestMapping(path = "/stocks/", method = RequestMethod.GET)
-	    public Quote findByFirstNameLastName(String symbol ) {
-	        return stockDataService.getStockBySymbol(symbol);
-	    }
-	    
-	    @RequestMapping(path="stocks/all", method = RequestMethod.GET)
-	    public Collection<Quote> stocks() {
-	    	return stockDataService.stocks();
-	    }
-	    @RequestMapping(path="/sectors/all", method = RequestMethod.GET)
-	    public Collection<Sector> sectors() throws JsonParseException, JsonMappingException, IOException {
-	    	return stockDataService.sectors();
-	    }
-	    
-	    @RequestMapping(path="/stocks/prices", method = RequestMethod.GET)
-	    public Collection<Quote> stocksWithPriceFilter(double price) {
-	    	return stocks().stream().filter(s->Double.parseDouble(s.getLow()) > price).collect(Collectors.toList());
-	    }
+	private StockDataService stockDataService;
+
+	@RequestMapping(path = "/stocks/", method = RequestMethod.GET)
+	public Quote stockBySymbol(String symbol) {
+		return stockDataService.getStockBySymbol(symbol);
+	}
+
+	@RequestMapping(path = "stocks/all", method = RequestMethod.GET)
+	public Collection<Quote> stocks() {
+		return stockDataService.stocks();
+	}
+
+	@RequestMapping(path = "/sectors/all", method = RequestMethod.GET)
+	public Collection<Sector> sectors() throws JsonParseException, JsonMappingException, IOException {
+		return stockDataService.sectors();
+	}
+
+	@RequestMapping(path = "/stocks/prices", method = RequestMethod.GET)
+	public Collection<Quote> stocksWithPriceFilter(double price) {
+		return stocks().stream().filter(s -> Double.parseDouble(s.getLow()) > price).collect(Collectors.toList());
+	}
 
 }
