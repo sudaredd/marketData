3a32b69be930aa0272e3c677f8fa2b1d6fb8f677
Sudarsana Kasireddy <sudaredd@gmail.com>
2021-01-24 14:13:36 -0500

    Upgrade spring version and fix compilation errors

--- marketData/src/test/java/market/data/MarketDataApplicationTests.java
+++ marketData/src/test/java/market/data/MarketDataApplicationTests.java
@@ -1,15 +1,11 @@
 package market.data;
 
 import market.MarketDataApplication;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @EnableAutoConfiguration
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes=MarketDataApplication.class)
 public class MarketDataApplicationTests {
 
